b11152a5555fa6bf571d9d3144203c06717d746b
Daniel Mateos Labrador <daniel.mat.lab@gmail.com>
2022-03-29 13:14:00 +0000

    Add todos api with fixed tests

--- .learn/exercises/07-post_todo/test.py
+++ .learn/exercises/07-post_todo/test.py
@@ -97,5 +97,5 @@ def test_add_new_todo():
 
 @pytest.mark.it("The endpoint POST /todos should exist")
 def test_return(client):
-    response = client.post('/todos')
+    response = client.post('/todos', json={"hello":"world"})
     assert response.status_code in [200, 201]
